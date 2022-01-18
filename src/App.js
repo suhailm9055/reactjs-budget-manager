67c638776fbeb40231dfc3b3d42b30b6da6d2cc0
suhaiLap-hp <suhailm9055@gmail.com>
2022-01-18 23:30:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Button, Container, Stack } from "react-bootstrap";
+import BudgetCard from "./component/BudgetCard";
+import AddBudgetModal from "./component/AddBudgetModal";
+import { useBudgets } from "./context/BudgetContext";
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets, getBudgetExpenses } = useBudgets();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container  className=" my-4 col-md-10 col-lg-8"  >
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        ></div>
+        {budgets.map((budget) => {
+          const amount = getBudgetExpenses(budget.id).reduce(
+            (total,expense)=>total+expense.amount,0
+          )
+          return(
+          <BudgetCard
+            key={budget.id}
+            name={budget.name}
+            amount={amount}
+            max={budget.max}
+          ></BudgetCard>
+        )})}
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => {
+          setShowAddBudgetModal(false);
+        }}
+      />
+    </>
   );
 }
 
---
+++ src/context/BudgetContext.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import useLocalStorage from "../hooks/useLocalStorage";
+const BudgetsContext = React.createContext();
+
+export function useBudgets() {
+  return useContext(BudgetsContext);
+}
+
+// {
+//     id:
+//     name:
+//     max:
+// }
+
+// {
+//     id:
+//     budgetId:
+//     amount:
+//     description:
+// }
+export const BudgetsProvider = ({ children }) => {
+  const [budgets, setBudgets] = useLocalStorage("budgets",[]);
+  const [expenses, setExpenses] = useLocalStorage("expenses",[]);
+
+  function getBudgetExpenses({ budgetId }) {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  }
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+  function deleteBudget({ id }) {
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id);
+    });
+  }
+  function deleteExpense({ id }) {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expenses) => expenses.id !== id);
+    });
+  }
+  return (
+    <BudgetsContext.Provider
+      value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+      }}
+    >
+      {children}
+    </BudgetsContext.Provider>
+  );
+};
